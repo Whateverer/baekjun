3b9078be3998b543c4a06b2d84bfcae3c957cd03
whatever <gksmf6699@naver.com>
2022-12-28 12:54:47 +0900

    B15596

---
+++ src/baekjun/level/function/B15596.java
@@ -0,0 +1,15 @@
+package baekjun.level.function;
+
+public class B15596 {
+
+}
+
+class Test {
+	public long sum(int[] a) {
+		long sum = 0;
+		for(int i : a) {
+			sum += i;
+		}
+		return sum; 
+	}
+}
