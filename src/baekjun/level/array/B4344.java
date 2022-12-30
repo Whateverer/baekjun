d3b44a845e144c41d5c5f941c1098712eae3b587
whatever <gksmf6699@naver.com>
2022-12-30 09:01:52 +0900

    B4344

---
+++ src/baekjun/level/array/B4344.java
@@ -0,0 +1,31 @@
+package baekjun.level.array;
+
+import java.util.Scanner;
+
+// 평균은 넘겠지
+public class B4344 {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int c = Integer.parseInt(sc.nextLine());
+		for(int i=0; i<c; i++) {
+			int cnt = sc.nextInt();
+			int[] scores = new int[cnt];
+			int sum = 0;
+			int upAvgCnt = 0;
+			for(int j=0; j<cnt; j++) {
+				scores[j] = sc.nextInt();
+				sum += scores[j];
+			}
+			double avg = (double) sum / cnt;
+			for(int score : scores) {
+				if(score > avg) {
+					upAvgCnt++;
+				}
+			}
+			double upAvg = (double) upAvgCnt / cnt * 100.0;  
+			
+			System.out.println(String.format("%.3f", upAvg) + "%");
+		}
+	}
+}
