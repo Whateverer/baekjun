141d0f646f46fc4b4315ffe8c50f0f255910a471
whatever <gksmf6699@naver.com>
2023-01-16 12:38:49 +0900

    B11654

---
+++ src/baekjun/level/string/B11654.java
@@ -0,0 +1,14 @@
+package baekjun.level.string;
+
+import java.util.Scanner;
+
+public class B11654 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		char ch = sc.nextLine().charAt(0);
+		int result = (int)ch;
+		System.out.println(result);
+	}
+
+}
