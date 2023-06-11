38746147d640b667bd6c49f9e92d9f9e3780ca63
yujin <hsjes777@gmail.com>
2023-06-11 23:50:24 +0900

    B2750 - 동빈나 선택정렬 추가

--- src/baekjun/level/sort/B2750.java
+++ src/baekjun/level/sort/B2750.java
@@ -11,7 +11,25 @@ public class B2750 {
         for (int i = 0; i < count; i++) {
             array[i] = sc.nextInt();
         }
-        Arrays.stream(array).sorted().forEach(System.out::println);
+//        Arrays.stream(array).sorted().forEach(System.out::println);
 
+        // 동빈나 선택정렬
+        for (int i = 0; i < count; i++) {
+            int min = 1001;
+            int index = 0;
+            for (int j = i; j < count; j++) {
+                if (min > array[j]) {
+                    min = array[j];
+                    index = j;
+                }
+            }
+            int temp = array[i];
+            array[i] = array[index];
+            array[index] = temp;
+        }
+        for (int i = 0; i < count; i++) {
+            System.out.println(array[i]);
+        }
     }
+
 }
